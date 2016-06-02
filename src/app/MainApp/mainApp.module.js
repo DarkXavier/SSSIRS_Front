0ee5ced2d595a4bb32a85f1c0e588b436a91c7bc
DarkXavier <pacoshola07@hotmail.com>
2016-06-01 21:13:21 -0500

    Add files via upload

---
+++ src/app/MainApp/mainApp.module.js
@@ -0,0 +1,13 @@
+/**
+ * Created by DarkXavier 30.05.16
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('app.mainApp', [
+            'app.mainApp.Solicitudes'
+
+        ]);
+
+})();
